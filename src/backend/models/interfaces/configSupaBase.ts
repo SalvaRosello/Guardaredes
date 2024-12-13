f14b2868cf6d0cda832c8d7062fb2df25c81d88e
Salva Rosello <salvarogo99@gmail.com>
2024-12-13 10:53:15 +0100

    Change DB to postgresql

--- src/backend/models/interfaces/configSupaBase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export function createUser() {
-    const SUPABASE_URL = process.env.SUPABASE_URL || '';
-    const SUPABASE_KEY = process.env.SUPABASE_KEY || '';
-    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-}
