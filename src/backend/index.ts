effad62db190ca47eda579db83c4e741a7395b01
Salva Rosello <salvarogo99@gmail.com>
2025-01-02 18:07:25 +0100

    Fix: routes and register js error

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,21 +2,27 @@ import express from 'express';
 import apiRouter from './routes/apiRouter.js';
 import { publicPath } from './configuration/config.js';
 import { staticRouter } from './routes/staticRouter.js';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+ 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+ 
 const app = express();
 const port = 3000;
-
+ 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(publicPath));
-
+app.use(express.static(path.join(__dirname, '../public/scripts')));
+ 
 app.use("/", staticRouter);
 app.use("/api/v1", apiRouter);
-
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
+ 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.error(err.stack);
   res.status(500).json({
